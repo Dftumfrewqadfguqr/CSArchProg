e82f5a18fd22099e3a5221b18975515e67b30358
Dftumfrewqadfguqr <gftumfrewqadfguqr@yandex.ru>
2024-06-03 12:27:34 +0700

    test

--- src/Server/JsonCast/JsonCast.cpp
+++ src/Server/JsonCast/JsonCast.cpp
@@ -1,18 +1,15 @@
 
 #include "JsonCast.hpp"
 
-void to_json(nlohmann::json &j, const User &user)
-{
-    j = nlohmann::json
-            {
+void to_json(nlohmann::json &j, const User &user) {
+    j = nlohmann::json {
                     {"id", user.uuid},
                     {"login", user.login},
                     {"access", User::AccessToStr(user.access)}
             };
 }
 
-void from_json(const nlohmann::json &j, User &user)
-{
+void from_json(const nlohmann::json &j, User &user) {
     j.at("login").get_to(user.login);
     j.at("password").get_to(user.password);
 
